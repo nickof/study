aeb40b2ff2dbaf6906a513eab17087726e21c7a4
nickof <752412348@qq.com>
2021-02-25 23:40:50 +0800

    update note
    update note

--- Android/itheima/day17jni/代码/20_自定义类型.c
+++ Android/itheima/day17jni/代码/20_自定义类型.c
@@ -1,6 +1,20 @@
 #include<stdlib.h>  
 #include<stdio.h>  
 typedef int i;
+
+//用法
+typedef struct Student{
+	short age;
+	char gender;
+	double score;
+	void(*p)();
+} stud;
+
+//用法jni,传参的时候可以增加代码的可读性
+typedef void* jobject;
+typedef jobject jclass;
+typedef jobject jstring;
+
 main(){ 
       i j = 123;
       printf("j = %d\n",j);
